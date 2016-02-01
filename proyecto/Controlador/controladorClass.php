f293a125a65ec60570e8d2b3d18102fff9d73926
Jon Sanchez Chica <jonsanchez19@gmail.com>
2016-02-01 10:44:34 +0100

    Resolver problema de creacion de directorios.

---
+++ proyecto/Controlador/controladorClass.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonsa
+ * Date: 01/02/2016
+ * Time: 10:42
+ */
+
+namespace proyecto\Controlador;
+
+
+class controladorClass
+{
+
+}
---
+++ proyecto/Vista/alumnoViews.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonsa
+ * Date: 01/02/2016
+ * Time: 10:43
+ */
+
+namespace proyecto\Vista;
+
+
+class alumnoViews
+{
+
+}
