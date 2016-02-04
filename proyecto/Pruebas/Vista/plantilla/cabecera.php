6e70bd5deb1e40e7c3911c857bf5be05d8775122
Martin Bengoa <martinbengoacasado@gmail.com>
2016-02-04 09:45:22 +0100

    Modificar proyecto

--- proyecto/Pruebas/Vista/plantilla/cabecera.php
+++ proyecto/Pruebas/Vista/plantilla/cabecera.php
@@ -11,12 +11,11 @@ require_once __DIR__."/../View.php";
     <link rel="stylesheet" type="text/css" href="<?php echo View::getUrlRaiz(); ?>/Vista/plantilla/css/style.css"
     media="screen" />
     <script language="JavaScript" type="text/javascript" src="<?php echo View::getUrlRaiz(); ?>/Vista/plantilla/js/validaciones.js"></script>
+        <script src="<?php echo View::getUrlRaiz(); ?>/Vista/plantilla/jquery-1.113.min"></script>
         <script type="text/javascript" src="<?php echo View::getUrlRaiz(); ?>/Vista/plantilla/jquery-1.10.2.js"></script>
-
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
         <link rel="stylesheet" type="text/css" href="<?php echo View::getUrlRaiz(); ?>/Vista/plantilla/bootstrap-3.3.6-dist/css/bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo View::getUrlRaiz(); ?>/Vista/plantilla/bootstrap-3.3.6-dist/css/bootstrap-theme.min.css"/>
-
     </head>
 <body>
 <header>
--- proyecto/Pruebas/Vista/plantilla/js/validaciones.js
+++ proyecto/Pruebas/Vista/plantilla/js/validaciones.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
     $('#insertar').submit(function(e){
@@ -23,33 +22,3 @@ $(document).ready(function(){
 
 });
 
-function comprobar_alta()
-{
-    var dato = document.getElementById('dni').value;
-
-    if (dato == "")
-    {
-        alert("DNI obligatorio...");
-        return;
-    }
-    dato = document.getElementById('nombre').value;
-
-    if (dato == "")
-    {
-        alert("Nombre obligatorio...");
-        return;
-    }
-    document.FNuevo.submit();
-}
-
-function comprobar_baja()
-{
-    var dato = document.getElementById('dni').value;
-
-    if (dato == "")
-    {
-        alert("DNI obligatorio...");
-        return;
-    }
-    document.FEliminar.submit();
-}
