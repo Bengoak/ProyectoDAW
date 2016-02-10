fabfe87b84bc606db4d41ba6c137ae6ba5b9d15a
Jon Sanchez Chica <jonsanchez19@gmail.com>
2016-02-10 12:24:10 +0100

    Prueba conflicto mismo fichero.

--- proyecto/Vista/Vista.php
+++ proyecto/Vista/Vista.php
@@ -1,18 +1,13 @@
 <?php
-
 require_once __DIR__."/View.php";
-
 class Vista extends View
 {
     public static function empezar(){
-
         require_once __DIR__ . "/plantilla/cabecera.php";
-
-
-
+            ?>
+            <p>Hola</p>
+            <?php
         require_once __DIR__ . "/plantilla/pie.php";
-
     }
 }
-
 ?>
