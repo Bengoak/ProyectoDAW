dc7f62b9e4a7b9d2880c7f1f89c66268ccef3076
Martin Bengoa <martinbengoacasado@gmail.com>
2016-02-10 12:26:17 +0100

    Commit

--- proyecto/Vista/Vista.php
+++ proyecto/Vista/Vista.php
@@ -1,18 +1,14 @@
 <?php
-
 require_once __DIR__."/View.php";
-
 class Vista extends View
 {
     public static function empezar(){
-
         require_once __DIR__ . "/plantilla/cabecera.php";
-
-
-
+            ?>
+            <p>hola</p>
+            <?php
         require_once __DIR__ . "/plantilla/pie.php";
 
     }
 }
-
 ?>
